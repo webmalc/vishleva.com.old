db1302095944dbc94bbbddb3d03b72e8c165c87c
webmalc <webmalc@gmail.com>
2017-04-11 15:49:33 +0300

    add test for batch sms sending

--- events/test/test_views.py
+++ events/test/test_views.py
@@ -1,9 +1,11 @@
-from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
-from django.conf import settings
-from vishleva.lib.test import ViewTestCase, LiveTestCase
-from events.models import Event
 import arrow
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+
+from events.models import Event
+from mailing.models import Sms
+from vishleva.lib.test import LiveTestCase, ViewTestCase
 
 
 class EventsViewTest(ViewTestCase):
@@ -18,6 +20,21 @@ class EventsViewTest(ViewTestCase):
         Test payments list unauthorized view
         """
         self._test_unauthorized_view('admin:events_calendar')
+
+    def test_admin_sms_form(self):
+        """ Test batch sms send to clients """
+        self._login_superuser()
+        url = reverse('admin:send_sms')
+        response = self.client.get(url + '?ids=1,2,3')
+        self.assertContains(response, 'id="id_message"')
+        form = response.context['form']
+        data = form.initial
+        text = 'test sms message from form'
+        data['message'] = text
+        response = self.client.post(url + '?ids=1,2,3', data)
+        sms = Sms.objects.all()
+        self.assertEqual(3, len(sms))
+        self.assertEqual(text, sms[0].text)
 
     def test_calendar(self):
         """
@@ -34,9 +51,11 @@ class EventsViewTest(ViewTestCase):
         self._login_superuser()
         url = reverse('admin:events_calendar')
         response = self.client.get(url)
-        self._is_succesful(response, title='Events calendar | Django site admin')
+        self._is_succesful(
+            response, title='Events calendar | Django site admin')
         self.assertContains(response, 'test_title_now')
-        self.assertContains(response, 'events-calendar-date">' + now.format('D MMM').lower())
+        self.assertContains(
+            response, 'events-calendar-date">' + now.format('D MMM').lower())
 
     def test_calendar_with_params(self):
         """
@@ -49,21 +68,26 @@ class EventsViewTest(ViewTestCase):
         url = reverse('admin:events_calendar')
 
         # test end query
-        response = self.client.get("{}?end={}".format(url, now.format('YYYY-MM-DD')))
+        response = self.client.get(
+            "{}?end={}".format(url, now.format('YYYY-MM-DD')))
         self.assertNotContains(response, tag + now.format('D MMM').lower())
-        self.assertContains(response, tag + now.replace(days=-period).format('D MMM').lower())
-        self.assertNotContains(response, tag + now.replace(days=-(period+1)).format('D MMM').lower())
+        self.assertContains(
+            response, tag + now.replace(days=-period).format('D MMM').lower())
+        self.assertNotContains(
+            response,
+            tag + now.replace(days=-(period + 1)).format('D MMM').lower())
 
         # test begin query
         end = now.replace(days=+period)
-        response = self.client.get("{}?begin={}".format(url, end.format('YYYY-MM-DD')))
+        response = self.client.get(
+            "{}?begin={}".format(url, end.format('YYYY-MM-DD')))
         self.assertNotContains(response, tag + now.format('D MMM').lower())
         self.assertContains(response, tag + end.format('D MMM').lower())
-        self.assertNotContains(response, tag + end.replace(days=-1).format('D MMM').lower())
+        self.assertNotContains(
+            response, tag + end.replace(days=-1).format('D MMM').lower())
 
 
 class EventsLiveTests(LiveTestCase):
-
     def test_calendar(self):
         """
         Test calendar page
@@ -84,18 +108,24 @@ class EventsLiveTests(LiveTestCase):
         self.selenium.get(self.live_server_url + url)
 
         assert 'test_title_now' in self.selenium.page_source
-        assert 'events-calendar-date">' + now.format('D MMM').lower() in self.selenium.page_source
+        assert 'events-calendar-date">' + now.format(
+            'D MMM').lower() in self.selenium.page_source
 
         begin = self.selenium.find_element_by_css_selector(
-            'td[data-time="{}"][data-date="{}"]'.format(begin_time, now.format('YYYY-MM-DD'))
-        )
+            'td[data-time="{}"][data-date="{}"]'.format(
+                begin_time, now.format('YYYY-MM-DD')))
         begin.click()
         end = self.selenium.find_element_by_css_selector(
-            'td[data-time="{}"][data-date="{}"]'.format(end_time, tomorrow.format('YYYY-MM-DD'))
-        )
+            'td[data-time="{}"][data-date="{}"]'.format(
+                end_time, tomorrow.format('YYYY-MM-DD')))
         end.click()
 
-        assert now.format('YYYY-MM-DD') == self.selenium.find_element_by_id('id_begin_0').get_attribute('value')
-        assert begin_time == self.selenium.find_element_by_id('id_begin_1').get_attribute('value')
-        assert tomorrow.format('YYYY-MM-DD') == self.selenium.find_element_by_id('id_end_0').get_attribute('value')
-        assert end_time == self.selenium.find_element_by_id('id_end_1').get_attribute('value')
+        assert now.format('YYYY-MM-DD') == self.selenium.find_element_by_id(
+            'id_begin_0').get_attribute('value')
+        assert begin_time == self.selenium.find_element_by_id(
+            'id_begin_1').get_attribute('value')
+        assert tomorrow.format(
+            'YYYY-MM-DD') == self.selenium.find_element_by_id(
+                'id_end_0').get_attribute('value')
+        assert end_time == self.selenium.find_element_by_id(
+            'id_end_1').get_attribute('value')
