7d3218216bdb3b3f90cc580abb0c7dca0a8064d4
webmalc <webmalc@gmail.com>
2017-05-04 15:48:36 +0300

    add special banner to index page

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,10 @@
 from django.contrib.flatpages.models import FlatPage
-from django.utils.translation import ugettext_lazy as _
 from django.db import models
-from django.template.defaultfilters import truncatewords, strip_tags
-from photologue.models import Gallery
+from django.template.defaultfilters import strip_tags, truncatewords
+from django.utils.translation import ugettext_lazy as _
 from ordered_model.models import OrderedModel
+from photologue.models import Gallery
+
 from vishleva.models import CommonInfo
 
 
@@ -11,6 +12,7 @@ class GalleryExtended(models.Model):
     gallery = models.OneToOneField(Gallery, related_name='extended')
     small_description = models.TextField()
     order = models.IntegerField(default=0)
+    is_special = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = u'Extra fields'
@@ -22,7 +24,8 @@ class GalleryExtended(models.Model):
 
 class ExtendedFlatPage(FlatPage):
     keywords = models.CharField(max_length=255, verbose_name=_('keywords'))
-    description = models.CharField(max_length=255, verbose_name=_('description'))
+    description = models.CharField(
+        max_length=255, verbose_name=_('description'))
 
 
 class Review(OrderedModel, CommonInfo):
@@ -30,12 +33,15 @@ class Review(OrderedModel, CommonInfo):
     Client reviews
     """
     text = models.TextField(db_index=True)
-    client = models.ForeignKey('events.Client', on_delete=models.SET_NULL, null=True, blank=False)
-    photo = models.ForeignKey('photologue.Photo', on_delete=models.SET_NULL, null=True, blank=True)
+    client = models.ForeignKey(
+        'events.Client', on_delete=models.SET_NULL, null=True, blank=False)
+    photo = models.ForeignKey(
+        'photologue.Photo', on_delete=models.SET_NULL, null=True, blank=True)
     is_enabled = models.BooleanField(default=True)
 
     def admin_thumbnail(self):
         return self.photo.admin_thumbnail() if self.photo else None
+
     admin_thumbnail.short_description = _('Photo')
     admin_thumbnail.allow_tags = True
 
@@ -48,5 +54,3 @@ class Review(OrderedModel, CommonInfo):
 
     class Meta(OrderedModel.Meta):
         pass
-
-
